1c576b9084bac65912c10413c3107931d266a62c
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-09-04 15:32:43 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+app = express();
+
+app.use(express.static("public"));
+
+app.listen( PORT, () => {
+    console.log(`Open on port ${PORT}`);
+})
