c913fd7795c610618f3c0290da7a3f73a5ca172f
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-09-04 16:56:06 -0500

    Added in the post and get request, fixed a bug in the index.html that wouldn't allow for it to run properly

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3001;
 
 app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.listen( PORT, () => {
     console.log(`Open on port ${PORT}`);
 })
 
 app.get("/notes", (req, res) => {
-    res.sendFile("/public/notes.html");
+    res.sendFile(path.join( __dirname, "/public/notes.html"));
+})
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join( __dirname, "/db/db.json"))
+})
+
+app.post("/api/notes", (req, res) => {
+    fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        const parsedData = JSON.parse(data);
+        console.log(parsedData.push(req.body));
+        // console.log(typeof parsedData);
+        // console.log(parsedData)
+        fs.writeFile("db/db.json", JSON.stringify(parsedData), (err) => {
+            if (err) {
+                console.log(err)
+            } 
+            console.log("success")
+        })    
+        res.sendFile(path.join( __dirname, "/db/db.json"))
+    })
+
 })
